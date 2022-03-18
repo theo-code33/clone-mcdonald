6ed2906e2b186bd72dade62ade0085c54d098a4e
Théo Gillet <gillettheo1@gmail.com>
2022-03-18 22:59:44 +0100

    create card item for burger

--- src/components/CardItemBurger.vue
+++ src/components/CardItemBurger.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="card-item-burger">
-        <div class="left-part-card-burger">
+    <div class="card-burger">
+        <div class="card-burger-left">
             <img :src="img" :alt="name">
         </div>
-        <div class="right-part-card-burger">
+        <div class="card-burger-right">
             <h3>{{name}}</h3>
             <p>{{price}} €</p>
         </div>
@@ -26,3 +26,42 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+    .card-burger{
+        width: 250px;
+        height: 136px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        border: 1px solid #dcdcdc;
+        border-radius: 4px;
+        padding: 24px 16px;
+        cursor: pointer;
+        
+
+        &-left{
+            width: 64px;
+            margin-right: 12px;
+
+            img{
+                width: 100%;
+            }
+        }
+        &-right{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: flex-start;
+            text-transform: uppercase;
+
+            h3{
+                margin: 0;
+            }
+            p{
+                margin: 10px 0 0 0;
+            }
+
+        }
+    }
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="home">
     <h1>Clone App McDonald</h1>
+    <CardItemBurger
+      :name="$store.state.burger.bigMac.name"
+      :price='$store.state.burger.bigMac.price'
+      :img='$store.state.burger.bigMac.img'
+    />
   </div>
 </template>
 
 <script>
-
+import CardItemBurger from '../components/CardItemBurger.vue'
 export default {
   name: 'HomePage',
   components: {
-
+    CardItemBurger
+  },
+  methods: {
   }
+  
 }
 </script>
