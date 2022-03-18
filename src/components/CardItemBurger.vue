755b3956111592603213ba116472d1bde5df09a5
Théo Gillet <gillettheo1@gmail.com>
2022-03-18 22:17:04 +0100

    create structure of card

---
+++ src/components/CardItemBurger.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card-item-burger">
+        <div class="left-part-card-burger">
+            <img :src="img" :alt="name">
+        </div>
+        <div class="right-part-card-burger">
+            <h3>{{name}}</h3>
+            <p>{{price}} €</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardItemBurger',
+    props:{
+        name: {
+            type: String,
+        },
+        price: {
+            type: Number,
+        },
+        img: {
+            type: Object,
+        }
+    }
+}
+</script>
