87b3b79e774f6e1bdc60100c81bebacc7f91b396
Théo Gillet <gillettheo1@gmail.com>
2022-03-18 22:24:25 +0100

    create big mac data

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    burger: {
+      bigMac: {
+        name: 'Big Mac',
+        price: 4.70,
+        img: 'https://ws.mcdonalds.fr/media/ff/62/d1/ff62d1acc3e68417ad9dd72e57257217e3e93f5d?auto=webp&width=1280&disable=upscale'
+      },
+    }
   },
   getters: {
   },
