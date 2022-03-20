68810ba01461feef9ecaab2326f0f28d1d259933
Théo Gillet <gillettheo1@gmail.com>
2022-03-20 12:44:32 +0100

    create component ModalBurger

---
+++ src/components/ModalBurger.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-modal">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ModalBurger',
+    props: {
+        name: {
+            type: String,
+        },
+        price: {
+            type: Number,
+        },
+        img: {
+            type: Object,
+        }
+    }
+}
+</script>
